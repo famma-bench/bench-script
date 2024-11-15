e2e0cab13611e6a9b74fb1a4e204fe28cad658cf
iLampard <siqiao_xue@163.com>
2024-11-15 14:49:29 +0800

    clean code

--- utils/gen_utils.py
+++ utils/gen_utils.py
@@ -4,20 +4,17 @@ import random
 import re
 import numpy as np
 import pandas as pd
-import logging
 import time
 import dictdatabase as DDB
 
-from tqdm import tqdm
-from models import get_client_fn
 from utils.data_utils import format_options, read_json, remove_json_format, default_response
 from utils.prompt_utils import MultipleChoiceQuestionPrompt, OpenQuestionPrompt
+from easyllm_kit.utils.io_utils import initialize_database, write_to_database
+from easyllm_kit.utils import get_logger
 from easyllm_kit.models import LLM
 from easyllm_kit.configs import Config
 
-
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
+logger = get_logger('famma', 'famma.log')
 
 
 def prepare_dataset_for_generation(data_dir, subset_name=None, question_ids=None):
@@ -115,18 +112,18 @@ def get_question_response(model, context, question_df, images, question_type="mu
 
     # we use litellm api for all the models
     msg = [{
-                "type": "text",
-                "text": input_prompt,
-            }
-            ]
+        "type": "text",
+        "text": input_prompt,
+    }
+    ]
     for image in images:
         msg.append({
-                    "type": "image_url",
-                    "image_url": {
-                        "url": f"data:image/jpeg;base64,{image}"
-                    },
-                })
-    
+            "type": "image_url",
+            "image_url": {
+                "url": f"data:image/jpeg;base64,{image}"
+            },
+        })
+
     model_output = model.generate(msg)
     return model_output
 
@@ -153,7 +150,7 @@ def safe_parse_response(response):
             }
             return response
         else:
-            logging.warning(
+            logger.warning(
                 "Response is not valid JSON or is malformed. Response: %s", response)
         return {}
 
@@ -178,10 +175,10 @@ def process_response(model_name, data_df, response, is_multiple_choice=False):
         response_answer = response_entry.get(
             f"answer-{index + 1}", default_response.get(language))
 
-        data_df.at[index, "model_answer"] = response_answer  
+        data_df.at[index, "model_answer"] = response_answer
         data_df.at[index, "model_explanation"] = response_entry.get(
             f"explanation-{index + 1}", default_response.get(language))
-        
+
         if is_multiple_choice:
             options = format_options(data_df.at[index, "options"])
             labels = options["labels"]
@@ -200,7 +197,7 @@ def generate_answer_by_model(model, sub_question_set_df, target_db_name, key):
     # Get the context from the first sub_question
     context = sub_question_set_df.iloc[0].get("context", "")
 
-   # Collect unique images
+    # Collect unique images
     images = []
     last_used_index = 0
     for _, row in sub_question_set_df.iterrows():
@@ -258,10 +255,14 @@ def generate_answer_by_model(model, sub_question_set_df, target_db_name, key):
         ["model_answer", "model_extract_answer", "model_explanation", "model_name"]]
     ddb_samples_dict = ddb_samples.to_dict(orient='records')
 
-    with DDB.at(target_db_name).session() as (sess, obj):
-        obj[key] = [{"model_answer": row["model_answer"], "model_extract_answer": row["model_extract_answer"], "model_explanation": row["model_explanation"], "model_name": row["model_name"]}
-                    for row in ddb_samples_dict]
-        sess.write()
+    # with DDB.at(target_db_name).session() as (sess, obj):
+    #     obj[key] = [{"model_answer": row["model_answer"], "model_extract_answer": row["model_extract_answer"], "model_explanation": row["model_explanation"], "model_name": row["model_name"]}
+    #                 for row in ddb_samples_dict]
+    #     sess.write()
+    input_dict = [{"model_answer": row["model_answer"], "model_extract_answer": row["model_extract_answer"],
+                   "model_explanation": row["model_explanation"], "model_name": row["model_name"]}
+                  for row in ddb_samples_dict]
+    write_to_database(target_db_name, key, input_dict)
 
     output_samples_subset = pd.concat(
         [multiple_choice_result, open_question_result], ignore_index=True, sort=False)
@@ -283,7 +284,7 @@ def save_output_samples(output_samples, model_name, save_dir):
     # Save the DataFrame to a CSV file
     output_samples.to_csv(output_file_path, index=False,
                           encoding='utf_8_sig', header=True)
-    logging.info("Saved output samples to %s", output_file_path)
+    logger.info("Saved output samples to %s", output_file_path)
 
 
 def generate_ans(config_dir, data_dir, save_dir, question_ids):
@@ -300,9 +301,11 @@ def generate_ans(config_dir, data_dir, save_dir, question_ids):
 
     # Initialize the DDB
     target_db_name = f'{model_name}_DDB'
-    init_db = DDB.at(target_db_name).read()
-    if init_db is None:
-        DDB.at(target_db_name).create()
+    # init_db = DDB.at(target_db_name).read()
+    # if init_db is None:
+    #     DDB.at(target_db_name).create()
+
+    initialize_database(output_db=target_db_name)
 
     # 2. Load the dataset
     dataset = prepare_dataset_for_generation(
@@ -312,7 +315,7 @@ def generate_ans(config_dir, data_dir, save_dir, question_ids):
     output_samples = pd.DataFrame()
     grouped = dataset.groupby(['language', 'main_question_id'])
 
-    for (language, main_question_id), sub_question_set_df in tqdm(grouped):
+    for (language, main_question_id), sub_question_set_df in grouped:
         key = str(f"{language}_{main_question_id}")
         try:
             # Attempt to read existing data from the DDB
@@ -325,7 +328,8 @@ def generate_ans(config_dir, data_dir, save_dir, question_ids):
                 sub_question_set_df = sub_question_set_df.reset_index(
                     drop=True).copy()
                 sub_question_set_df.loc[:, [
-                    "model_answer", "model_extract_answer", "model_explanation", "model_name"]] = existing_data
+                                               "model_answer", "model_extract_answer", "model_explanation",
+                                               "model_name"]] = existing_data
                 output_samples = pd.concat(
                     [output_samples, sub_question_set_df], ignore_index=True)
             else:
@@ -336,7 +340,7 @@ def generate_ans(config_dir, data_dir, save_dir, question_ids):
                     [output_samples, output_samples_subset], ignore_index=True, sort=False)
 
         except Exception as e:
-            logging.error(
+            logger.error(
                 "Error processing main_question_id %s: %s", main_question_id, str(e))
             time.sleep(2)
             continue
@@ -344,5 +348,5 @@ def generate_ans(config_dir, data_dir, save_dir, question_ids):
         # Avoid hitting rate limits or overwhelming the API
         time.sleep(2)
 
-    logging.info('Generation complete')  # logger
+    logger.info('Generation complete')  # logger
     save_output_samples(output_samples, model_name, save_dir)
