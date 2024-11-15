2483c55cc3d1630bde91ee422c83168fd4f18e3d
iLampard <siqiao_xue@163.com>
2024-11-15 11:34:38 +0800

    remove subset_name in args

--- step_1_download_dataset.py
+++ step_1_download_dataset.py
@@ -10,12 +10,12 @@ if __name__ == "__main__":
     parser.add_argument("--hf_dir", type=str, default="weaverbirdllm/famma",
                         help="The dir of dataset on huggingface.")
 
-    parser.add_argument("--subset_name", type=str, default=None,
-                        help="English / Chinese / French, there are three subset. If None, download all the subset.")
+    parser.add_argument("--split", type=str, default=None,
+                        help="validation / test, there are two splits. If None, download all the splits.")
 
     parser.add_argument("--save_dir", type=str, default="./data",
                         help="The local dir to save the dataset.",)
 
     args = parser.parse_args()
 
-    download_data(args.hf_dir, args.subset_name, args.save_dir)
+    download_data(args.hf_dir, args.split, args.save_dir)
