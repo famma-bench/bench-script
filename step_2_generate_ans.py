1f0811d453df8c125e8563a0ea96747797a8fbcd
iLampard <siqiao_xue@163.com>
2025-01-18 12:42:16 +0800

    fix the interface of generating answers script

--- step_2_generate_ans.py
+++ step_2_generate_ans.py
@@ -1,6 +1,6 @@
 import argparse
-from utils.data_utils import parse_list
-from utils.gen_utils import generate_ans
+from omegaconf import OmegaConf
+from famma_runner.runners import Runner
 
 if __name__ == "__main__":
     """
@@ -9,20 +9,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--config_dir", type=str, default="./gen_config.yaml",
-                        help="The dir of model config file.")
-
-    parser.add_argument("--data_dir", type=str,
-                        default="./data", help="The parent dir of dataset")
-
-    parser.add_argument("--question_ids", type=parse_list, default=None,
-                        help="list of question ids to query. If None, we will run over all the questions in the subset")
-
-    parser.add_argument("--save_dir", type=str, default="./result",
-                        help="The local dir to save the generation result")
+                        help="The dir of generation config file.")
 
     args = parser.parse_args()
 
-    generate_ans(args.config_dir, 
-                 args.data_dir,
-                 args.save_dir, 
-                 args.question_ids)
+    config = OmegaConf.load(args.config_dir)
+
+    runner = Runner.build_from_config(config)
+
+    runner.run()    
