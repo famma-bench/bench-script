763d9f72f006610657895ff25c8c45aa6f4edc1b
iLampard <siqiao_xue@163.com>
2025-04-25 11:48:52 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="famma_runner",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(),
     install_requires=[
         "easyllm_kit",
